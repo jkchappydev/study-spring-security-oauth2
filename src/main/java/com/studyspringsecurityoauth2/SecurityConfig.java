cccbf713ca91f8d5b6e1f750e93d83a4643f3be0
jkchappydev <jkchappydev@gmail.com>
2025-07-05 19:13:07 +0900

    study: section2 - 초기화 과정 이해 - SecurityBuilder / SecurityConfigurer

---
+++ src/main/java/com/studyspringsecurityoauth2/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.studyspringsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .anyRequest().authenticated());
+
+        // spring security 6.2 부터는 두번째 인자로 Configurer customizer (람다식) 요구 (첫번째 Configurer 인스턴스)
+        http.with(new CustomSecurityConfigurer(), config -> config.setFlag(false)); // with() 사용: apply()는 spring security 6.2에서 deprecated 됨
+        return http.build();
+    }
+
+}
