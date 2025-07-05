8fe563ebf607e75b6584d010a28169034b903a13
jkchappydev <jkchappydev@gmail.com>
2025-07-05 18:02:17 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/studyspringsecurityoauth2/StudySpringSecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.studyspringsecurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringSecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringSecurityOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/studyspringsecurityoauth2/StudySpringSecurityOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyspringsecurityoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringSecurityOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
