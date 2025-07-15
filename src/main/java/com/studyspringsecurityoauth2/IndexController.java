14caed87f212e348368258106c0ce792ef5debbf
jkchappydev <jkchappydev@gmail.com>
2025-07-16 00:14:44 +0900

    study: section8 - API 커스텀 구현 - Authorization BaseUrl & Redirection BaseUrl

--- src/main/java/com/studyspringsecurityoauth2/IndexController.java
+++ src/main/java/com/studyspringsecurityoauth2/IndexController.java
@@ -1,11 +1,5 @@
 package com.studyspringsecurityoauth2;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,32 +9,6 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
-    }
-
-    // 표준 방식
-    @GetMapping("/user")
-    public OAuth2User user(Authentication auth) {
-        // 직접 SecurityContext 로 부터 인증객체를 가져온다.
-        // OAuth2AuthenticationToken authentication1 = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-
-        // 메서드에 선언된 참조를 가져온다.
-        OAuth2AuthenticationToken authentication2 = (OAuth2AuthenticationToken) auth;
-        return authentication2.getPrincipal();
-    }
-
-    // 어노테이션 방식
-    @GetMapping("/oauth2User")
-    public OAuth2User oAuth2User(@AuthenticationPrincipal OAuth2User oAuth2User) {
-        System.out.println("oAuth2User = " + oAuth2User);
-        return oAuth2User;
-    }
-
-    // oidc 방식
-    // OidcUser 는 OAuth2User 를 상속받기 때문에, OAuth2User 로도 참조가 가능하지만 명확하게 작성하는 것이 좋다.
-    @GetMapping("/oidcUser")
-    public OidcUser oidcUser(@AuthenticationPrincipal OidcUser oidcUser) {
-        System.out.println("oAuth2User = " + oidcUser);
-        return oidcUser;
     }
 
 }
--- src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
@@ -2,42 +2,33 @@ package com.studyspringsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
-    private final ClientRegistrationRepository clientRegistrationRepository;
-
-    public OAuth2ClientConfig(ClientRegistrationRepository clientRegistrationRepository) {
-        this.clientRegistrationRepository = clientRegistrationRepository;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
-                .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler())
-                        .invalidateHttpSession(true)
-                        .clearAuthentication(true)
-                        .deleteCookies("JSESSIONID"));
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login/v2/oauth2/code/*")
+                        .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
+                                .baseUri("/oauth2/v1/authorization")
+                        )
+                        .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
+                                .baseUri("/login/v1/oauth2/code/*")
+                        )
+                );
 
         return http.build();
-    }
-
-    private LogoutSuccessHandler oidcLogoutSuccessHandler() {
-        OidcClientInitiatedLogoutSuccessHandler successHandler
-                = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
-        successHandler.setPostLogoutRedirectUri("http://localhost:8081/login");
-        return successHandler;
     }
 
 }
