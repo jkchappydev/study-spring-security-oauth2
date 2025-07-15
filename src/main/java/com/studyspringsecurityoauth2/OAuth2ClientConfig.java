8990932fef56b5bfc4d09d5e155ee9ff1dd1b3f0
jkchappydev <jkchappydev@gmail.com>
2025-07-16 03:03:29 +0900

    study: section8 - API 커스텀 구현 - OAuth2AuthorizationRequestResolver

--- src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
@@ -4,31 +4,43 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class OAuth2ClientConfig {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public OAuth2ClientConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/home").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login/v2/oauth2/code/*")
                         .authorizationEndpoint(authorizationEndpointConfig -> authorizationEndpointConfig
-                                .baseUri("/oauth2/v1/authorization")
+                                .authorizationRequestResolver(customOAuth2AuthorizationRequestResolver())
                         )
-                        .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
-                                .baseUri("/login/v1/oauth2/code/*")
-                        )
-                );
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/home")
+                )
+        ;
 
         return http.build();
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestResolver customOAuth2AuthorizationRequestResolver() {
+        return new CustomOAuth2AuthorizationRequestResolver(clientRegistrationRepository, "/oauth2/authorization");
     }
 
 }
