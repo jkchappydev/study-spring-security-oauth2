90bdd7cfd17f87e33ea5e0f15c54619f4854dc5a
jkchappydev <jkchappydev@gmail.com>
2025-07-20 18:54:22 +0900

    study: section10 - OAuth 2.0 Social Login 연동 구현 - (1)

--- src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/com/studyspringsecurityoauth2/OAuth2ClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -12,10 +13,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class OAuth2ClientConfig {
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/static/js/**", "/static/images/**", "/static/css/**","/static/scss/**");
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                .anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults());
+        http
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/logout"))
+        ;
 
         return http.build();
     }
